cf648c755aa62265c964dac994d5b3c173efd3a3
Jaime <jaimemoralmillan@digitechfp.com>
2025-05-11 16:49:02 +0200

    redirected root

--- questify_backend/urls.py
+++ questify_backend/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 from rest_framework.routers import DefaultRouter
 from api.views import TaskViewSet
 
@@ -7,6 +8,7 @@ router = DefaultRouter()
 router.register(r'tasks', TaskViewSet, basename='task')
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/tasks/', permanent=False)), 
     path('admin/', admin.site.urls),
     path('api/',include(router.urls))
 ]
